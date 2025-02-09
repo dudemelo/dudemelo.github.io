84a492e5590b12f4f8144342ef942c6f7213fb78
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-02-09 17:22:44 +0100

    It was supposed to be fake text bug for now it's just square

--- main.mjs
+++ main.mjs
@@ -1,5 +1,10 @@
 "use strict";
 
+const canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = 400;
+const context = canvas.getContext("2d");
+
 class ScrambleText {
   /** @param {HTMLElement} el **/
   constructor(el) {
@@ -40,27 +45,71 @@ class Scramble {
   }
 }
 
-class TShape {
+class Vector {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+  /** @param {Vector} other **/
+  add(other) {
+    this.x += other.x;
+    this.y += other.y;
+  }
+}
+
+class FakeText {
   constructor() {
-    /** @type {HTMLCanvasElement} **/
-    const canvas = document.getElementById("tshape");
-    canvas.width = window.innerWidth;
-    canvas.height = 400;
-    const context = canvas.getContext("2d");
+    this.reset();
+  }
+  reset() {
+    this.size = Math.random() * (20 - 5) + 5;
+    this.color = `rgba(0, 255, 100, ${Math.random() * (0.5 - 0.1) + 0.1})`;
+    this.velocity = new Vector(
+      Math.random() * 0.5 * (-1 * Math.random()),
+      Math.random() * 0.5 * (-1 * Math.random()),
+    );
+    this.position = new Vector(
+      Math.random() * canvas.width,
+      Math.random() * canvas.height,
+    );
+  }
+  update() {
+    if (
+      this.position.x + this.size < 0 ||
+      this.position.x > canvas.width ||
+      this.position.y + this.size < 0 ||
+      this.position.y > canvas.height
+    ) {
+      this.reset();
+    }
+    this.position.add(this.velocity);
     context.beginPath();
-    context.fillStyle = "white";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = this.color;
+    context.fillRect(this.position.x, this.position.y, this.size, this.size);
     context.closePath();
   }
-  update() {}
+}
+
+class FuturisticCanvas {
+  constructor() {
+    /** @type {FakeText[]} **/
+    this.texts = [];
+    for (let i = 0; i < 20; i++) {
+      this.texts.push(new FakeText());
+    }
+  }
+  update() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.texts.forEach((text) => text.update());
+  }
 }
 
 const scramble = new Scramble();
-const tshape = new TShape();
+const futuristic = new FuturisticCanvas();
 
 function mainLoop() {
   scramble.update();
-  tshape.update();
+  futuristic.update();
   requestAnimationFrame(mainLoop);
 }
 
